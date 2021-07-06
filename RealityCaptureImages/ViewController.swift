493109d9e075308930caaa12671a9e626cbe5f2f
史威 <3091892@qq.com>
2021-07-06 10:01:10 +0800

    Initial Commit

---
+++ RealityCaptureImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealityCaptureImages
+//
+//  Created by sytz on 2021/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
